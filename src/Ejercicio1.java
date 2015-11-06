4ddffce39b51dcade9b78cbacd879110c8f3c457
barni <velascod898@gmail.com>
2015-11-06 17:22:26 +0100

    Proyecto Inicial
    Esta clase solo tiene un mensaje

---
+++ src/Ejercicio1.java
@@ -0,0 +1,10 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+
+public class Ejercicio1 extends acm.program.GraphicsProgram{
+	
+public void run(){
+	GLabel etiqueta=new GLabel("Hola Git!!");
+	add(etiqueta, 120, 130);
+}
+}
