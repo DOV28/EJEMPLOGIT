ff9069823a34b1de6dbc9dd9220588a3d3ad75c1
barni <velascod898@gmail.com>
2015-11-10 17:46:38 +0100

    Terminada la pirámide y es invertida

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -13,12 +13,21 @@ public void pintaPiramide(){
 	int numLadrillosBase=14;
 	int anchoLadrillo=30;
 	int altoLadrillo=12;
+	int altoPiramide=numLadrillosBase*altoLadrillo;
+	int basePiramide=numLadrillosBase*anchoLadrillo;
+	int desplazamientoVertical=getHeight()-altoPiramide;
+	
 	for (int j=0;j<numLadrillosBase;j++){
 		
 	for(int i=0;i<numLadrillosBase-j;i++){
 		GRect ladrillo=new GRect(anchoLadrillo, altoLadrillo);
-		add(ladrillo,anchoLadrillo*j/2+ anchoLadrillo*i, altoLadrillo*j);
+		
+		add(ladrillo,getWidth()/2-basePiramide/2+anchoLadrillo*j/2+getHeight()-altoLadrillo - altoLadrillo*j, eventMask);
+		
+		pause(20);
 										}
 							}
 					}
+
+
 }
